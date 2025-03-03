f567e45c70d3c4b01806903b0041a3c46d4d6a22
Ethan <Ethan.lynn2027@gbwl.org>
2025-03-03 17:11:32 +0000

    added stuff

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+/*This is a MULTI-LINE comment in JS
+Below is an example of a JS STATEMENT
+(instructiion/command)
+Statements end with a SEMICOLON (;)
+ */
+console.log("Jalen Brunson");
+
+//This is a SINGLE-LINE comment
+//console.log is a FUNCTION to display imformation
+console.log("this is the console/output");
+
+//DECLARE a VARIABLE to hold data
+// Use the LET keyword to create the variable
+let pokemon;
+
+// ASSIGN a VALUE to that variable
+// Like packing an item into a box
+pokemon = "Pikachu";
+
+// Can DECLARE + ASSIGN in one statement
+let name = "Ethan"
